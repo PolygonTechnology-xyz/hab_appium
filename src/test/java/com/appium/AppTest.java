bf163bf1af984b3c8b5a76d64541fb2a95543665
jarin <jarin@polygontech.xyz>
2023-10-17 18:22:37 +0600

    login

--- src/test/java/com/appium/AppTest.java
+++ src/test/java/com/appium/AppTest.java
@@ -1,40 +1,80 @@
 package com.appium;
 
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+/* import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder; */
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class AppTest {
 
-  private AndroidDriver driver;
+  public AndroidDriver driver;
+  
 
+
+  
   @Before
-  public void setUp() throws MalformedURLException {
+  public void configAppium() throws MalformedURLException, InterruptedException {
+
+	/* UiAutomator2Options options = new UiAutomator2Options();
+	options.setDeviceName("Pixel 6 API 30"); 
+		options.setApp(System.getProperty("user.dir")+"/hab/src/test/java/com/appium/resources/hab_driver.apk");
+	AndroidDriver driver = new AndroidDriver(new URL("http://192.168.12.172:4723/wd/hub"), options);
+	/home/jarin/document/workspace/appium/hab/hab/src/test/java/com/appium/resources/hab_driver.apk
+ */
+
+
+	//create capabilities
     DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
     desiredCapabilities.setCapability("platformName", "android");
     desiredCapabilities.setCapability("appium:platformVersion", "11");
     desiredCapabilities.setCapability("appium:deviceName", "Pixel 6 API 30");
     desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
-    desiredCapabilities.setCapability("appium:app", "/home/jarin/Desktop/hab_driver.apk");
+    desiredCapabilities.setCapability("appium:app", "/home/jarin/Desktop/knower-dev-debug.apk");
     desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
     desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
     desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
     desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
 
-    driver = new AndroidDriver(new URL("http://192.168.12.172:4723/wd/hub"), desiredCapabilities);
+	//create object for android
+
+    driver = new AndroidDriver (new URL("http://192.168.12.172:4723/wd/hub"), desiredCapabilities);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
   }
 
   @Test
-  public void sampleTest() {
+  public void Login() {
 
+    driver.findElement(By.className("android.widget.CheckBox")).click();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+    driver.findElement(AppiumBy.accessibilityId("Googleでログイン")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
   }
 
-  @After
+  /* @After
   public void tearDown() {
-    // driver.quit();
-  }
+    driver.quit();
+  } */
 }
+
+
+
