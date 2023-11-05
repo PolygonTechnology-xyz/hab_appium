33648279b725ed79341b5e5e0539bab3ea5d523b
jarin <jarin@polygontech.xyz>
2023-11-05 14:40:38 +0600

    index fix

--- src/test/java/com/appium/AppTest.java
+++ src/test/java/com/appium/AppTest.java
@@ -16,51 +16,51 @@ public class AppTest {
   @Test
   public void Login() throws InterruptedException, MalformedURLException {
 
-      AppiumDriver <WebElement> driver = AppiumDriverCapabilities.getAppiumDriver();
-      
-      WebElement forgetPass = driver.findElement(By.xpath("//android.view.View[@index = '5']"));
-      forgetPass.click();
-      Thread.sleep(2000);
+        AppiumDriver <WebElement> driver = AppiumDriverCapabilities.getAppiumDriver();
+        
+        WebElement forgetPass = driver.findElement(By.xpath("//android.view.View[@index = '5']"));
+        forgetPass.click();
+        Thread.sleep(2000);
 
-      WebElement backToLogin = driver.findElement(By.xpath("//android.view.View[@index = '3']"));
-      Thread.sleep(2000);
-      backToLogin.click();
-      Thread.sleep(3000);
+        WebElement backToLogin = driver.findElement(By.xpath("//android.view.View[@index = '3']"));
+        Thread.sleep(2000);
+        backToLogin.click();
+        Thread.sleep(3000);
 
-      WebElement useridTextBox = driver.findElement(By.xpath("//android.widget.EditText[@index = '2']"));
-      useridTextBox.click();
-      Thread.sleep(2000);
-      useridTextBox.sendKeys("+8107000000007");
-      Thread.sleep(2000);
+        WebElement useridTextBox = driver.findElement(By.xpath("//android.widget.EditText[@index = '2']"));
+        useridTextBox.click();
+        Thread.sleep(2000);
+        useridTextBox.sendKeys("+8107000000007");
+        Thread.sleep(2000);
 
-      WebElement passwordTextBox = driver.findElement(By.xpath("//android.widget.EditText[@index = '4']")); 
-      passwordTextBox.click();
-      Thread.sleep(2000);
-      passwordTextBox.sendKeys("1234567*");
-      Thread.sleep(3000);
+        WebElement passwordTextBox = driver.findElement(By.xpath("//android.widget.EditText[@index = '4']")); 
+        passwordTextBox.click();
+        Thread.sleep(2000);
+        passwordTextBox.sendKeys("1234567*");
+        Thread.sleep(3000);
 
-      WebElement viewPass = driver.findElement(By.xpath("//android.widget.ImageView[@index = '0']")); 
-      viewPass.click();
-      Thread.sleep(3000);
+        WebElement viewPass = driver.findElement(By.xpath("//android.widget.ImageView[@index = '0']")); 
+        viewPass.click();
+        Thread.sleep(3000);
 
-      WebElement loginButton = driver.findElement(By.xpath("//android.view.View[@index = '6']"));
-      loginButton.click();
-      Thread.sleep(3000);
+        WebElement loginButton = driver.findElement(By.xpath("//android.view.View[@index = '6']"));
+        loginButton.click();
+        Thread.sleep(3000);
 
-      WebElement nextButton1 = driver.findElement(By.xpath("//android.view.View[@content-desc=\"次へ進む\"]"));
-      nextButton1.click();
-      Thread.sleep(2000);
-      
-      WebElement nextButton2 = driver.findElement(By.xpath("//android.view.View[@index = '2']"));
-      nextButton2.click();
-      Thread.sleep(2000);
+        WebElement nextButton1 = driver.findElement(By.xpath("//android.view.View[@content-desc=\"次へ進む\"]"));
+        nextButton1.click();
+        Thread.sleep(2000);
+        
+        WebElement nextButton2 = driver.findElement(By.xpath("//android.view.View[@index = '2']"));
+        nextButton2.click();
+        Thread.sleep(2000);
 
-      WebElement nextButton3 = driver.findElement(By.xpath("//android.view.View[@index = '5']"));
-      nextButton3.click();
-      Thread.sleep(2000);
+        WebElement nextButton3 = driver.findElement(By.xpath("//android.view.View[@index = '5']"));
+        nextButton3.click();
+        Thread.sleep(2000);
 
 
-      Dimension windowSize = driver.manage().window().getSize();
+        Dimension windowSize = driver.manage().window().getSize();
         int screenHeight = windowSize.getHeight();
         int screenWidth = windowSize.getWidth();
 
